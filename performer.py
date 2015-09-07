3a282c3a3019b1290f417a02145bdc6dee8a987a
dvagoga <dvagoga@gmail.com>
2015-09-07 09:59:18 +0300

    add: parser for task file

--- performer.py
+++ performer.py
@@ -13,6 +13,19 @@ def getTask():
         taskList = taskFile.readlines()
 
     res['name'] = taskList[0][0:-1]
+    for i in range(1, len(taskList)):
+        words = taskList[i].split()
+        if words[0] == 'level:':
+            res['level'].append({'o':[], 'f':[], 'cell':[]})
+        elif words[0] == 'o:':
+            res['level'][-1]['o'] += words[1:]
+        elif words[0] == 'f:':
+            res['level'][-1]['f'] += words[1:]
+        elif words[0] == 'cell:':
+            res['level'][-1]['cell'].append({'intersection':words[1:], 'f':[]})
+        elif words[0] == 'p:':
+            res['level'][-1]['cell'][-1]['f'].append({'index':words[1], 'operands':words[2:]})
+
     return res
 
 def createFolder(dirName):
@@ -82,6 +95,9 @@ def addJsLib(levels):
             libIndex += 1
         return res
 
+    for l in levels:
+        print(l)
+
     with open(JS_LIBRARY_FILE_NAME, 'r') as jsLibFile:
         libCode = jsLibFile.readlines()
 
@@ -91,12 +107,17 @@ def addJsLib(levels):
     for name in selectedNames:
         newCode += codeFoundInLib(libCode, name)
 
+    print(newCode)
+
+    #with open(jsFileName, 'a') as jsFile:
+    #    for line in newCode:
+    #        jsFile.write(line)
+
     
 ############################################################
 
 
 task = getTask()
-print(task['name'])
 createFolder(task['name'])
 createHtml(task['name'])
 createJs(task['name'])
